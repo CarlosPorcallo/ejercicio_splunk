2a620f83845ff4fa4ab85d90197428a187391175
Carlos Porcallo <2172021691@alumnos.xoc.uam.mx>
2025-01-17 08:08:31 -0600

    Se agregan los archivos al repositorio.

---
+++ build.bat
@@ -0,0 +1,8 @@
+docker container stop splunk
+docker container rm splunk
+docker run -d ^
+    -p 8000:8000 ^
+    -e "SPLUNK_START_ARGS=--accept-license" ^
+    -e "SPLUNK_PASSWORD=<password>" ^
+    --name splunk ^
+    splunk/splunk:latest
