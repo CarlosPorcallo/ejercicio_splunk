e4516877ccce32d44b71a14b80a990a25e2d9f42
Carlos Porcallo <2172021691@alumnos.xoc.uam.mx>
2025-01-17 17:11:18 -0600

    Ajustes para las variables de entorno en Windows.

--- build.bat
+++ build.bat
@@ -1,8 +1,17 @@
+rem ###### Variables de entorno ######
+set SPLUNK_PASSWORD=<password>
+set CONTAINER_NAME=<splunk>
+set DOCKER_IMG="<splunk/splunk:latest>"
+set DOCKER_NETWORK="<docker-network>"
+rem ###### Variables de entorno ######
+
 docker container stop splunk
 docker container rm splunk
+docker network create %DOCKER_NETWORK%
 docker run -d ^
+    --network=%DOCKER_NETWORK% ^
     -p 8000:8000 ^
     -e "SPLUNK_START_ARGS=--accept-license" ^
-    -e "SPLUNK_PASSWORD=<password>" ^
-    --name splunk ^
-    splunk/splunk:latest
+    -e "SPLUNK_PASSWORD=%SPLUNK_PASSWORD%" ^
+    --name %CONTAINER_NAME% ^
+    %DOCKER_IMG%
